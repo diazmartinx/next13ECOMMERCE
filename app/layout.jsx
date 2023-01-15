33e6abf1a848b71ed990be06ec6f0f0b5ec2c5b3
Martin Diaz <midiazgt@gmail.com>
2023-01-15 01:45:19 -0300

    add category active link

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,24 @@
 import './globals.css'
 import Header from '../components/server/header'
-import { Categories } from '../components/server/categories'
+import Categories from '../components/client/categories'
 
+async function getCategories() {
+  return fetch('https://dummyjson.com/products/categories')
+  .then((res) => res.json())
+}
 
-export default function RootLayout({
-  children,
-}) {
-  
+export default async function RootLayout({ children }) {
+  const categories = await getCategories();
   return (
     <html lang="en">
       <head />
       <body className='bg-slate-50'>
         <Header />
         <section className="grid grid-cols-[200px_minmax(900px,_1fr)]">
-          <Categories />
+          <Categories data={categories} />
           {children}
-
-        </section>
-        
-        </body>
+        </section>        
+      </body>
     </html>
   )
 }
--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   images: {
     domains: ['i.dummyjson.com'],
   },
+  reactStrictMode: false
 }
 
 module.exports = nextConfig
